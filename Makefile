0da2cf1795b7f5dfee3b5dc8a113ae0a853cc87e
Hugo MARQUET <humarque@e2r5p9.42.fr>
2019-04-12 14:58:39 +0200

    retry

---
+++ Makefile
@@ -0,0 +1,53 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    Makefile                                           :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: humarque <marvin@42.fr>                    +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2019/04/08 14:15:22 by humarque          #+#    #+#              #
+#    Updated: 2019/04/09 14:54:20 by humarque         ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
+NAME = fillit
+CC = gcc
+
+SRC_PATH = ./src/
+OBJ_PATH = ./obj/
+LIB_PATH = ./lib/
+INC_PATH = ./include/ $(LIB_PATH)libft/include/
+
+GCC_FLGS = -Werror -Wextra -Wall
+
+SRC_NAME = main.c ft_check_shape.c ft_coord.c ft_parser.c ft_printresult.c \
+		   ft_solver.c ft_stocklist.c ft_stocktetra.c
+
+OBJ_NAME = $(SRC_NAME:.c=.o)
+LIB_NAME = libft
+
+SRC = $(addprefix $(SRC_PATH), $(SRC_NAME))
+OBJ = $(addprefix $(OBJ_PATH), $(OBJ_NAME))
+INC = $(addprefix -I,$(INC_PATH))
+LIB = $(addprefix -L$(LIB_PATH),$(LIB_NAME))
+
+all: $(NAME)
+
+$(NAME): $(OBJ)
+	make -C $(LIB_PATH)libft
+	$(CC) $(GCC_FLGS) $(LIB) -lft $(INC) $(OBJ) -o $(NAME)
+
+$(OBJ_PATH)%.o: $(SRC_PATH)%.c
+	mkdir -p $(OBJ_PATH)
+	$(CC) $(GCC_FLGS) $(INC) -o $@ -c $<
+
+clean:
+	rm -fv $(OBJ)
+	make clean -C $(LIB_PATH)libft
+
+
+fclean: clean
+	make -C $(LIB_PATH)libft fclean
+	rm -fv $(NAME)
+
+re: fclean all
---
+++ src/ft_stocktetra.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stocktetra.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: humarque <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/07 15:51:09 by humarque          #+#    #+#             */
+/*   Updated: 2019/04/12 14:55:00 by humarque         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/fillit.h"
+
+char		**ft_malloc_tab(char **tab, int count)
+{
+	int	i;
+	int	j;
+	int	num;
+
+	i = 0;
+	j = 0;
+	num = count / 5 + 1;
+	if (!(tab = (char **)malloc(sizeof(char *) * ((num + 1)))))
+		return (NULL);
+	while (i < num)
+	{
+		if (!(tab[i] = (char *)ft_memalloc(sizeof(char*) * 21)))
+		{
+			while (--i)
+			{
+				free(tab[i]);
+			}
+			free(tab);
+			return (NULL);
+		}
+		i++;
+	}
+	tab[i] = 0;
+	return (tab);
+}
+
+void		free_square(t_square *square)
+{
+	int i;
+
+	i = 0;
+	while (i < square->size)
+	{
+		ft_memdel((void **)&(square->square[i]));
+		i++;
+	}
+	ft_memdel((void **)&(square->square));
+	ft_memdel((void **)&(square));
+}
+
+static char	**ft_tabcreate(char *str, char **tab, int bloc)
+{
+	int		i;
+	int		j;
+	char	*s2;
+
+	i = 0;
+	s2 = ft_memalloc(sizeof(char) * 21);
+	printf("%d", bloc);
+	tab = (char **)ft_memalloc(sizeof(char*) * (bloc + 1));
+	while (i < 3)
+	{
+		j = 0;
+		s2 = ft_strncpy(s2, str, 16);
+		if (!(tab[i] = (char*)malloc(sizeof(char *) * 21)))
+			return (NULL);
+		tab[i] = ft_strdup(s2);
+		tab[i][17] = '\0';
+		while (j++ < 16)
+			str++;
+		i++;
+		ft_strclr(s2);
+	}
+	free(s2);
+	tab[i] = 0;
+	return (tab);
+}
+
+char		**ft_stocktetra(int fd, int count)
+{
+	char	**tab;
+	int		i;
+	char	*str;
+	char	*line;
+
+	tab = NULL;
+	i = 0;
+	if (!(str = (char*)ft_memalloc(sizeof(char) * 21)))
+		return (NULL);
+	while (get_next_line(fd, &line) == 1)
+	{
+		if (line[0] != '\0')
+			str = ft_strjoin(str, line);
+		free(line);
+	}
+	if (!(tab = ft_tabcreate(str, tab, count)))
+		return (NULL);
+	return (tab);
+}
